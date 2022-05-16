b7a348cbbe5d9bf1ed3d309b7e0fba119831a235
slimane-IA <72779962+slimane-IA@users.noreply.github.com>
2022-05-16 17:17:59 +0200

    addinf dfs algorithm to find an existing path

--- src/dm1.ml
+++ src/dm1.ml
@@ -56,11 +56,85 @@ let printMatrix matrix n =
 ;;
 
 
+
+let voisin g vus n x y =
+  let res = [(x,y+1);(x,y-1);(x+1,y);(x-1,y)] in 
+  let rec loop res=
+    match res with 
+      [] -> []
+      |(x,y):: rres -> 
+        if ( x>=n || x<0 || y>=n || y<0)  then loop rres
+        else 
+          if g.(x).(y) = 0 || vus.(x).(y) = true then loop rres
+          else (x,y)::(loop rres)
+    in
+    loop res
+;; 
+
+
+
+
+
+
+
+
+let rec return_valid_path voisin_path paire =
+  match voisin_path with 
+  [] -> (false,[])
+  |(bol,list)::listlist -> 
+    if bol then (bol,paire::list )
+    else return_valid_path listlist paire
+;;
+
+
+let posCmp x y  x2 y2 = 
+  ( (x=x2) && (y=y2) ) 
+;;  
+
+
+(* explore the matrix
+  each step ahead save to path
+  step back delete path 
+  voisin = +-1xy unless =0
+  if end = start return path and end all
+  *)
+let rec explore_find_path terrain x1 y1 x2 y2 =
+  let n = Array.length terrain in
+  let vus = Array.make_matrix n n false in
+  let rec loop x y =
+    if ( posCmp x y  x2 y2) then (true,[(x,y)])
+    else 
+      begin  
+        vus.(x).(y) <- true ;
+        let voisin_path = List.map ( fun v ->  loop (fst v) (snd v) ) (voisin terrain vus n x y ) in
+        (return_valid_path voisin_path (x,y)) 
+      end 
+  in
+  snd (loop x1 y1) 
+;;
+
+
+
+
+
+
+let print_path p =
+  List.iter (fun (x, y) -> Printf.printf "(%d, %d) " x y) p;
+  Printf.printf "\n"
+
+;;
+
+
+
+
 (* testing *)
 let listTerrain,n = (load "terrain.txt") in 
 
 let terrain = repTerrain n listTerrain in 
-printMatrix terrain n 
+printMatrix terrain n;
+print_path (explore_find_path terrain 2 0 4 3);;
+
+(* end testing *)
 
 
 
