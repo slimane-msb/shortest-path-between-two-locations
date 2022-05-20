87f3745c2271ed03eb7b3754e558539bc1003841
slimane-IA <72779962+slimane-IA@users.noreply.github.com>
2022-05-20 21:20:04 +0200

    test version 2 part 1

--- src/version1Test.ml
+++ src/version1Test.ml
@@ -1,18 +1,22 @@
  
 open Version1 ;;
+open Printf;;
 
-Printf.printf "testing version1\n";
+printf "\n********* testing version1 ***********\n\n";
+
+printf "\n********* rectangles ***********\n\n";
 let listTerrain, n = (load "terrain.txt") in 
+List.iter (fun (x, y, dx, dy) -> printf "%d %d %d %d\n" x y dx dy ) listTerrain;
 
+printf "\n********* Matrice ***********\n\n";
 let terrain = repTerrain n listTerrain in 
+printMatrix terrain n; 
 
-printMatrix terrain n;
-
-Printf.printf "le chemin entre (%d,%d) et la dest (%d,%d) est : \n" 2 0 4 3;
+printf "\n********* chemin ***********\n\n";
+printf "le chemin entre (%d,%d) et la dest (%d,%d) est : \n" 2 0 4 3;
 let path = (explore_find_path terrain 2 0 4 3) in
 print_path path;
 
-Printf.printf "afficher le terrain avec chemin \n";
+printf "\n********* afficher le terrain avec chemin ***********\n\n";
 print_path_in_terrain terrain n path;
 
- 
