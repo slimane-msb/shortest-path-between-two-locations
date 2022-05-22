06440a37ab8d7d9e22585dd40aaa54effc6d2f3b
slimane-IA <72779962+slimane-IA@users.noreply.github.com>
2022-05-22 12:30:29 +0200

    adding field input file exemple

--- src/squelette-dm.ml
+++ src/squelette-dm.ml
@@ -26,46 +26,5 @@ let print_path p =
 
   
   
-(**
-   Fonction de lecture du fichier d'entrée
-     load: string -> (int * int) list * int
 
-   L'appel  (load f)  lit le fichier dont le nom est donné par la 
-   chaîne (f) et renvoie une paire  (murs, n)  où
-   - (murs) est la liste des quadruplets (x, y, dx, dy) donnant les
-     dimensions des r zones intraversables
-   - (n) est la longueur du côté du terrain
-   On suppose que le fichier (f) contient une description de terrain
-   valide. Le résultat de (load) n'est pas spécifié sinon.
- *)
-open Scanf
-let load file =
-  let c = Scanning.open_in file in
-  let n = bscanf c "%d\n" (fun n -> n) in
-  let r = bscanf c "%d\n" (fun r -> r) in
-  let murs = ref [] in
-  for _ = 1 to r do
-    bscanf c "%d %d %d %d\n" (fun x y dx dy -> murs := (x, y, dx, dy) :: !murs)
-  done;
-  !murs, n
 
-(**
-   Exemple d'ossature pour un programme complet, prenant un nom de fichier
-   sur la ligne de commande et affichant le chemin trouvé, ainsi que quelques
-   mesures de temps d'exécution.
-   Note : la mesure des temps d'exécution peut ne pas fonctionner sous windows.
- *)
-  
-let _ =
-  let file = Sys.argv.(1) in
-  let murs, n = load file in
-  let t1 = Unix.gettimeofday() in
-  let qt, k = numerote (list2qtree n murs) 0 in
-  let t2 = Unix.gettimeofday() in
-  let coords = mk_coords qt k n in
-  let g = mk_graph qt coords in
-  let t3 = Unix.gettimeofday() in
-  let path = find_path (n/2, 0) (n/2, n) (qt, n) (g, coords) in
-  let t4 = Unix.gettimeofday() in
-  printf "Temps:\n  construction du quadtree %fs\n  construction du graphe %fs\n  recherche de chemin %fs\n" (t2 -. t1) (t3 -. t2) (t4 -. t3);
-  print_path path
